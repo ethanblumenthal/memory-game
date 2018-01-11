584d0d038aaa98684406802174ce517e8f834777
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-11 12:58:06 -0800

    added initial Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React, {Componenet} from 'react';
+import PropTypes from 'prop-types';
+import './Navbar.css';
+
+const Navbar = (props) => {
+    <header>
+        <h2><a>Memory Game</a></h2>
+        <nav>
+            <li><a>New Game</a></li>
+        </nav>
+    </header>
+};
+
+Navbar.PropTypes = {};
+
+export default Navbar;
