6106699420adf547a9f5909f7a76ecb247e3c3a8
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-11 12:56:24 -0800

    added initial Card.js

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React, {Componenet} from 'react';
+import PropTypes from 'prop-types';
+import './Card.css';
+
+const Card = (props) => {
+    return (
+        <div />
+    );
+};
+
+Card.PropTypes = {};
+
+export default Card;
